752519c92b5389f61aa1406b63546ddbce3f7005
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-25 23:54:23 +0530

    add utils files

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	//console.log('running')
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.js to start working on your app!</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
