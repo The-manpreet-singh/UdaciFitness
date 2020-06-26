f22be77a7b568f469c351b2ce16d6ddaa106e59e
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-26 23:56:14 +0530

    add Add entry fuctionaality

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,48 +3,6 @@ import { View } from "react-native";
 import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
 import { white } from "./colors";
 
-export function isBetween(num, x, y) {
-	if (num >= x && num <= y) {
-		return true;
-	}
-
-	return false;
-}
-
-export function calculateDirection(heading) {
-	let direction = "";
-
-	if (isBetween(heading, 0, 22.5)) {
-		direction = "North";
-	} else if (isBetween(heading, 22.5, 67.5)) {
-		direction = "North East";
-	} else if (isBetween(heading, 67.5, 112.5)) {
-		direction = "East";
-	} else if (isBetween(heading, 112.5, 157.5)) {
-		direction = "South East";
-	} else if (isBetween(heading, 157.5, 202.5)) {
-		direction = "South";
-	} else if (isBetween(heading, 202.5, 247.5)) {
-		direction = "South West";
-	} else if (isBetween(heading, 247.5, 292.5)) {
-		direction = "West";
-	} else if (isBetween(heading, 292.5, 337.5)) {
-		direction = "North West";
-	} else if (isBetween(heading, 337.5, 360)) {
-		direction = "North";
-	} else {
-		direction = "Calculating";
-	}
-
-	return direction;
-}
-
-export function timeToString(time = Date.now()) {
-	const date = new Date(time);
-	const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
-	return todayUTC.toISOString().split("T")[0];
-}
-
 export function getMetricMetaInfo(metric) {
 	const info = {
 		run: {
@@ -52,7 +10,6 @@ export function getMetricMetaInfo(metric) {
 			max: 50,
 			unit: "miles",
 			step: 1,
-
 			type: "steppers",
 			getIcon() {
 				return (
@@ -121,4 +78,46 @@ export function getMetricMetaInfo(metric) {
 	};
 
 	return typeof metric === "undefined" ? info : info[metric];
+}
+
+export function isBetween(num, x, y) {
+	if (num >= x && num <= y) {
+		return true;
+	}
+
+	return false;
+}
+
+export function calculateDirection(heading) {
+	let direction = "";
+
+	if (isBetween(heading, 0, 22.5)) {
+		direction = "North";
+	} else if (isBetween(heading, 22.5, 67.5)) {
+		direction = "North East";
+	} else if (isBetween(heading, 67.5, 112.5)) {
+		direction = "East";
+	} else if (isBetween(heading, 112.5, 157.5)) {
+		direction = "South East";
+	} else if (isBetween(heading, 157.5, 202.5)) {
+		direction = "South";
+	} else if (isBetween(heading, 202.5, 247.5)) {
+		direction = "South West";
+	} else if (isBetween(heading, 247.5, 292.5)) {
+		direction = "West";
+	} else if (isBetween(heading, 292.5, 337.5)) {
+		direction = "North West";
+	} else if (isBetween(heading, 337.5, 360)) {
+		direction = "North";
+	} else {
+		direction = "Calculating";
+	}
+
+	return direction;
+}
+
+export function timeToString(time = Date.now()) {
+	const date = new Date(time);
+	const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+	return todayUTC.toISOString().split("T")[0];
 }
