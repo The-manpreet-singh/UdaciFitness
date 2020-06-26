bc455f9141beee0c4a7bf9d51df668eba874fe87
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-27 01:11:54 +0530

    add redux

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { View } from "react-native";
 import AddEntry from "./components/AddEntry";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
 
 export default function App() {
 	//console.log('running')
 	return (
-		<View>
-			<AddEntry />
-		</View>
+		<Provider store={createStore(reducer)}>
+			<View>
+				<AddEntry />
+			</View>
+		</Provider>
 	);
 }
