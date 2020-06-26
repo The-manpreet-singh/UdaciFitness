e6d9f1d897663d4dde8c68ee5391657dbe881106
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-27 01:02:01 +0530

    add api in addentry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,8 @@ import { View, TouchableOpacity, Text } from "react-native";
 import { getMetricMetaInfo, timeToString } from "../utils/helpers";
 import TextButton from "./TextButton";
 import { Ionicons } from "@expo/vector-icons";
+import { submitEntry, removeEntry } from '../utils/api'
+
 
 function SubmitBtn({ onPress }) {
 	return (
@@ -59,7 +61,7 @@ export default class AddEntry extends Component {
 		// Navigate to home
 
 		// Save to "DB"
-
+    submitEntry({ key, entry })
 		// Clear local notification
 	};
 
@@ -70,7 +72,8 @@ export default class AddEntry extends Component {
 
 		// Route to Home
 
-		// Update "DB"
+    // Update "DB"
+    removeEntry(key)
 	};
 
 	render() {
