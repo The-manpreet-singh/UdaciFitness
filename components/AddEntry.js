4b85d2bb95adc4d209cf0f610e0b5e1f1671e677
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-27 00:49:03 +0530

    add reset

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-
 import UdaciSlider from "./UdaciSlider";
 import UdaciSteppers from "./UdaciSteppers";
 import DateHeader from "./DateHeader";
 import { View, TouchableOpacity, Text } from "react-native";
 import { getMetricMetaInfo, timeToString } from "../utils/helpers";
+import TextButton from "./TextButton";
+import { Ionicons } from "@expo/vector-icons";
 
 function SubmitBtn({ onPress }) {
 	return (
@@ -62,8 +63,29 @@ export default class AddEntry extends Component {
 		// Clear local notification
 	};
 
+	reset = () => {
+		const key = timeToString();
+
+		// Update Redux
+
+		// Route to Home
+
+		// Update "DB"
+	};
+
 	render() {
 		const metaInfo = getMetricMetaInfo();
+
+		if (this.props.alreadyLogged) {
+			return (
+				<View>
+					<Ionicons name={"ios-happy"} size={100} />
+					<Text>You already logged your information for today.</Text>
+					<TextButton onPress={this.reset}>Reset</TextButton>
+				</View>
+			);
+		}
+
 		return (
 			<View>
 				<DateHeader date={new Date().toLocaleDateString()} />
