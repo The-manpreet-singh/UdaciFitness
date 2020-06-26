6c62e2762339c27788ea59071bca1e8d6c3dc4e1
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-26 23:16:35 +0530

    add helper function

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
 	//console.log('running')
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
+			<Text>Hello World app!</Text>
 		</View>
 	);
 }
