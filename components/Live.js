ab7b4721075aab10bcd3911a79876a65092f6621
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-28 01:05:46 +0530

    finish geolocation

--- components/Live.js
+++ components/Live.js
@@ -1,30 +1,81 @@
 import React, { Component } from "react";
 import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from "react-native";
 import { Foundation } from "@expo/vector-icons";
+
 import { purple, white } from "../utils/colors";
+import { calculateDirection } from "../utils/helpers";
+import * as Permissions from "expo-permissions";
+import * as Location from "expo-location";
 
 export default class Live extends Component {
 	state = {
 		coords: null,
-		status: "undetermined",
+		status: null,
 		direction: "",
 	};
-	askPermission = () => {};
+	componentDidMount() {
+		Permissions.getAsync(Permissions.LOCATION)
+			.then(({ status }) => {
+				if (status === "granted") {
+					return this.setLocation();
+				}
+
+				this.setState(() => ({ status }));
+			})
+			.catch((error) => {
+				console.warn("Error getting Location permission: ", error);
+
+				this.setState(() => ({ status: "undetermined" }));
+			});
+	}
+
+	askPermission = () => {
+		Permissions.askAsync(Permissions.LOCATION)
+			.then(({ status }) => {
+				if (status === "granted") {
+					return this.setLocation();
+				}
+
+				this.setState(() => ({ status }));
+			})
+			.catch((error) => console.warn("error asking Location permission: ", error));
+	};
+
+	setLocation = () => {
+		Location.watchPositionAsync(
+			{
+				enableHighAccuracy: true,
+				timeInterval: 1,
+				distanceInterval: 1,
+			},
+			({ coords }) => {
+				const newDirection = calculateDirection(coords.heading);
+				const { direction, bounceValue } = this.state;
+
+				this.setState(() => ({
+					coords,
+					status: "granted",
+					direction: newDirection,
+				}));
+			}
+		);
+	};
 	render() {
 		const { status, coords, direction } = this.state;
-
 		if (status === null) {
 			return <ActivityIndicator style={{ marginTop: 30 }} />;
 		}
-
 		if (status === "denied") {
 			return (
-				<View>
-					<Text>Denied</Text>
+				<View style={styles.center}>
+					<Foundation name="alert" size={50} />
+					<Text>
+						You denied your location. You can fix this by visiting your settings and enabling location services for this
+						app.
+					</Text>
 				</View>
 			);
 		}
-
 		if (status === "undetermined") {
 			return (
 				<View style={styles.center}>
@@ -36,11 +87,22 @@ export default class Live extends Component {
 				</View>
 			);
 		}
-
 		return (
 			<View style={styles.container}>
-				<Text>Live</Text>
-				<Text>{JSON.stringify(this.state)}</Text>
+				<View style={styles.directionContainer}>
+					<Text style={styles.header}>You're heading</Text>
+					<Text style={styles.direction}> {direction}</Text>
+				</View>
+				<View style={styles.metricContainer}>
+					<View style={styles.metric}>
+						<Text style={[styles.header, { color: white }]}>Altitude</Text>
+						<Text style={[styles.subHeader, { color: white }]}> {Math.round(coords.altitude * 3.2808)} Feet</Text>
+					</View>
+					<View style={styles.metric}>
+						<Text style={[styles.header, { color: white }]}>Speed</Text>
+						<Text style={[styles.subHeader, { color: white }]}>{(coords.speed * 2.2369).toFixed(1)} MPH</Text>
+					</View>
+				</View>
 			</View>
 		);
 	}
@@ -67,5 +129,38 @@ const styles = StyleSheet.create({
 	buttonText: {
 		color: white,
 		fontSize: 20,
+	},
+	directionContainer: {
+		flex: 1,
+		justifyContent: "center",
+	},
+	header: {
+		fontSize: 35,
+		textAlign: "center",
+	},
+	direction: {
+		color: purple,
+		fontSize: 120,
+		textAlign: "center",
+	},
+	metricContainer: {
+		flexDirection: "row",
+		justifyContent: "space-around",
+		backgroundColor: purple,
+	},
+	metric: {
+		flex: 1,
+		paddingTop: 15,
+		paddingBottom: 15,
+		backgroundColor: "rgba(255, 255, 255, 0.1)",
+		marginTop: 20,
+		marginBottom: 20,
+		marginLeft: 10,
+		marginRight: 10,
+	},
+	subHeader: {
+		fontSize: 25,
+		textAlign: "center",
+		marginTop: 5,
 	},
 });
