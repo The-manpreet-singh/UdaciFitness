50d24a9e895fc80c44fc2ff530cea9d733342b9e
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-27 23:59:28 +0530

    add entntydetails cards

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,25 +1,48 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { connect } from "react-redux";
+import { timeToString, getDailyReminderValue } from "../utils/helpers";
+import MetricCard from "./MetricCard";
+import { white } from "../utils/colors";
 
 class EntryDetail extends Component {
-  static navigationOptions = ({ navigation }) => {
-    const { entryId } = navigation.state.params
+	static navigationOptions = ({ navigation }) => {
+		const { entryId } = navigation.state.params;
 
-    const year = entryId.slice(0, 4)
-    const month = entryId.slice(5,7)
-    const day = entryId.slice(8)
+		const year = entryId.slice(0, 4);
+		const month = entryId.slice(5, 7);
+		const day = entryId.slice(8);
 
-    return {
-      title: `${month}/${day}/${year}`
-    }
-  }
+		return {
+			title: `${month}/${day}/${year}`,
+		};
+	};
 	render() {
+		const { metrics } = this.props;
 		return (
-			<View>
+			<View style={styles.container}>
+				<MetricCard metrics={metrics} />
 				<Text>Entry Detail - {JSON.stringify(this.props.navigation.state.params.entryId)}</Text>
 			</View>
 		);
 	}
 }
 
-export default EntryDetail;
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: white,
+		padding: 15,
+	},
+});
+
+function mapStateToProps(state, { navigation }) {
+	const { entryId } = navigation.state.params;
+
+	return {
+		entryId,
+		metrics: state[entryId],
+	};
+}
+
+export default connect(mapStateToProps)(EntryDetail);
