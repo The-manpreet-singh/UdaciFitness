7605750928983d63f15651a7df4282488aed9627
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-27 23:53:14 +0530

    add entry.js

--- components/History.js
+++ components/History.js
@@ -38,7 +38,7 @@ class History extends Component {
 					<Text style={styles.noDataText}>{today}</Text>
 				</View>
 			) : (
-				<TouchableOpacity onPress={() => console.log("Pressed!")}>
+				<TouchableOpacity onPress={() => this.props.navigation.navigate("EntryDetail", { entryId: key })}>
 					<MetricCard date={formattedDate} metrics={metrics} />
 				</TouchableOpacity>
 			)}
