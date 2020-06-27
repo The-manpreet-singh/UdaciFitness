52225c765d51011a5b3847056bc2f90d86dc1889
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-28 00:04:47 +0530

    add nVIgTION IN ADDENTRY

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -10,6 +10,7 @@ import { submitEntry, removeEntry } from "../utils/api";
 import { connect } from "react-redux";
 import { addEntry } from "../actions";
 import { purple, white } from "../utils/colors";
+import { NavigationActions } from "react-navigation";
 
 function SubmitBtn({ onPress }) {
 	return (
@@ -66,7 +67,7 @@ class AddEntry extends Component {
 		this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }));
 
 		// Navigate to home
-
+		this.toHome();
 		// Save to "DB"
 		submitEntry({ key, entry });
 		// Clear local notification
@@ -83,9 +84,12 @@ class AddEntry extends Component {
 			})
 		);
 		// Route to Home
-
+		this.toHome();
 		// Update "DB"
 		removeEntry(key);
+	};
+	toHome = () => {
+		this.props.navigation.dispatch(NavigationActions.back({ key: "AddEntry" }));
 	};
 
 	render() {
